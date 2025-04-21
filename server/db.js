9d90d600b67f95349cf6fc66552141cb88ca1b45
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-04-21 16:05:30 -0400

    added code for createTables in db.js

--- server/db.js
+++ server/db.js
@@ -1,6 +1,35 @@
 const { client } = require("./common");
 
-const createTables = async () => {};
+const createTables = async () => {
+  try {
+    await client.connect();
+    const SQL = `
+            DROP TABLE IF EXISTS Customer CASCADE;
+            CREATE TABLE Customer(
+                id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                name VARCHAR(100)
+            );
+            DROP TABLE IF EXISTS Restaurant CASCADE;
+            CREATE TABLE Restaurant(
+                id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                name VARCHAR(100)
+            );
+            DROP TABLE IF EXISTS Reservation CASCADE;
+            CREATE TABLE Reservation(
+                id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                date DATE NOT NULL,
+                party_count INTEGER NOT NULL,
+                restaurant_id UUID REFERENCES Restaurant(id) ON DELETE CASCADE ON UPDATE CASCADE NOT NULL,
+                customer_id UUID REFERENCES Customer(id) ON DELETE CASCADE ON UPDATE CASCADE NOT NULL
+            );
+        `;
+    await client.query(SQL);
+    console.log("We have seeded our db");
+    await client.end();
+  } catch (error) {
+    console.error(error);
+  }
+};
 const createCustomer = async () => {};
 const createRestaurant = async () => {};
 const fetchCustomers = async () => {};
