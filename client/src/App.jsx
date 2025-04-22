ef0022c764be7a2577a4469a485c2db1b9301cf0
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-04-22 10:13:52 -0400

    started app.jsx setup for UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,43 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <main></main>;
+  const [showRestaurant, setShowRestaurant] = useState(true);
+  if (showRestaurant) {
+    return (
+      <div className="page">
+        <div className="header">
+          <ul>
+            <li>
+              <button>Customers</button>
+            </li>
+            <li>
+              <button>Restaurants</button>
+            </li>
+          </ul>
+        </div>
+        <main>
+          <h1>The Acme Reservation Planner</h1>
+        </main>
+      </div>
+    );
+  }
+  return (
+    <div className="page">
+      <div className="header">
+        <ul>
+          <li>
+            <button>Customers</button>
+          </li>
+          <li>
+            <button>Restaurants</button>
+          </li>
+        </ul>
+      </div>
+      <main>
+        <h1>The Acme Reservation Planner</h1>
+      </main>
+    </div>
+  );
 }
 
 export default App;
